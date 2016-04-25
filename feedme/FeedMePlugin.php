207c59b1d24541e221c38d21e76c9eb5097c57e5
Josh Crawford <joshua@sgroup.com.au>
2016-04-25 19:12:53 +1000

    version 1.4.11

--- feedme/FeedMePlugin.php
+++ feedme/FeedMePlugin.php
@@ -24,7 +24,7 @@ class FeedMePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.4.10';
+        return '1.4.11';
     }
 
     public function getSchemaVersion()
