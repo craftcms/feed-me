6acacb016068fdfb90ec7e20a9d08edcd27cd284
Josh Crawford <joshua@sgroup.com.au>
2016-05-07 00:00:10 +1000

    added `CURLOPT_SSL_VERIFYPEER` for https feeds

--- feedme/services/FeedMe_FeedService.php
+++ feedme/services/FeedMe_FeedService.php
@@ -193,6 +193,7 @@ class FeedMe_FeedService extends BaseApplicationComponent
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($curl);
 
         if (!$response) {
