b33c7e7b047032d095d92596183d7f83a7ea1aa1
Josh Crawford <joshua@sgroup.com.au>
2018-08-16 17:30:32 +1000

    Matrix - Refactor parsing

--- src/fields/Matrix.php
+++ src/fields/Matrix.php
@@ -46,51 +46,51 @@ class Matrix extends Field implements FieldInterface
         // reflected in the field - phew!
         //
         // So, in order to keep data in the order provided in our feed, we start there (as opposed to looping through blocks)
-
+        $blockCount = 0;
+        $previousBlockHandle = '';
+    
         foreach ($this->feedData as $nodePath => $value) {
-            $blockCount = 0;
+            // Get the field mapping info for this node in the feed
+            $fieldInfo = $this->_getFieldMappingInfoForNodePath($nodePath, $blocks);
 
-            // Then, loop through all our block definitions to find the correct field definition
-            foreach ($blocks as $blockHandle => $blockInfo) {
-                $fields = Hash::get($blockInfo, 'fields');
+            // If this is data concerning our Matrix field and blocks
+            if ($fieldInfo) {
+                $blockHandle = $fieldInfo['blockHandle'];
+                $subFieldHandle = $fieldInfo['subFieldHandle'];
+                $subFieldInfo = $fieldInfo['subFieldInfo'];
+                $isComplexField = $fieldInfo['isComplexField'];
 
-                $blockCount++;
+                // Keep track of the previous block handle that we've got data for and matched. This helps to group items
+                // correctly that are grouped in the feed's structure, but would otherwise be tricky to ascertain
+                if ($previousBlockHandle !== $blockHandle) {
+                    $previousBlockHandle = $blockHandle;
+                    $blockCount++;
+                }
 
-                // Strip out array numbers in the feed path like: MatrixBlock/0/Images/0. We use this to get the field
-                // its supposed to match up with, which is stored in the DB like MatrixBlock/Images
-                $feedPath = preg_replace('/(\/\d+\/)/', '/', $nodePath);
-                $feedPath = preg_replace('/^(\d+\/)|(\/\d+)/', '', $feedPath);
+                $key = $blockCount . '.' . $blockHandle . '.' . $subFieldHandle;
 
-                // Find the field information we're mapping data to
-                foreach ($fields as $subFieldHandle => $subFieldInfo) {
-                    $node = Hash::get($subFieldInfo, 'node');
+                // Check for complex fields (think Table, Super Table, etc), essentially anything that has
+                // sub-fields, and doesn't have data directly mapped to the field itself. It needs to be
+                // accumulated here (so its in the right order), but grouped based on the field and block
+                // its in. A little bit annoying, but no better ideas...
+                if ($isComplexField) {
+                    $complexFields[$key]['info'] = $subFieldInfo;
+                    $complexFields[$key]['data'][$nodePath] = $value;
+                    continue;
+                }
 
-                    // Try to find the index of the Matrix Block this data sits under.
-                    // If this is a single Matrix block, it won't be an array (so it won't have an index)
-                    // So we force it to either get the MatrixBlock/1/Images index, or force it to '0'
-                    preg_match('/\/(\d+)\//', $nodePath, $matches);
-                    $blockIndex = Hash::get($matches, '1', $blockCount);
+                // Swap out the node-path stored in the field-mapping info, because
+                // it'll be generic MatrixBlock/Images not MatrixBlock/0/Images/0 like we need
+                $subFieldInfo['node'] = $nodePath;
+                
+                // Parse each field via their own fieldtype service
+                $parsedValue = $this->_parseSubField($this->feedData, $subFieldHandle, $subFieldInfo);
 
-                    // Check for complex fields (think Table, Super Table, etc), essentially anything that has
-                    // sub-fields, and doesn't have data directly mapped to the field itself. It needs to be
-                    // accumulated here (so its in the right order), but grouped based on the field and block
-                    // its in. A little bit annoying, but no better ideas...
-                    $fieldFieldHandles = Hash::extract($subFieldInfo, 'fields.{*}.node');
-
-                    if (in_array($feedPath, $fieldFieldHandles)) {
-                        $complexFields[$subFieldHandle . '_' . $blockIndex]['info'] = $subFieldInfo;
-                        $complexFields[$subFieldHandle . '_' . $blockIndex]['data'][$nodePath] = $value;
-                        continue;
-                    }
-
-                    if ($feedPath == $node) {
-                        // Before we do anything, we need to parse the inner-field data for each fieldtype
-                        // But we need to swap out the node-path stored in the field-mapping info, because
-                        // it'll be generic MatrixBlock/Images not MatrixBlock/0/Images/0 like we need
-                        $subFieldInfo['node'] = $nodePath;
-
-                        $fieldData = $this->_parseSubField($this->feedData, $subFieldHandle, $subFieldInfo, $blockIndex, $blockHandle, $fieldData);
-                    }
+                // Finish up with the content, also sort out cases where there's array content
+                if (isset($fieldData[$key])) {
+                    $fieldData[$key] = array_merge_recursive($fieldData[$key], $parsedValue);
+                } else {
+                    $fieldData[$key] = $parsedValue;
                 }
             }
         }
@@ -99,15 +99,22 @@ class Matrix extends Field implements FieldInterface
         // They have their mapping setup on sub-fields, and need to be processed all together, which we've already prepared.
         // Additionally, we only want to supply each field with a sub-set of data related to that specific block and field
         // otherwise, we get the field class processing all blocks in one go - not what we want.
-        foreach ($complexFields as $fieldHandleBlockIndex => $complexInfo) {
-            $parts = explode('_', $fieldHandleBlockIndex);
-            $subFieldHandle = $parts[0];
-            $blockIndex = $parts[1];
+        foreach ($complexFields as $key => $complexInfo) {
+            $parts = explode('.', $key);
+            $blockIndex = $parts[0];
+            $blockHandle = $parts[1];
+            $subFieldHandle = $parts[2];
 
             $subFieldInfo = Hash::get($complexInfo, 'info');
             $nodePaths = Hash::get($complexInfo, 'data');
 
-            $fieldData = $this->_parseSubField($nodePaths, $subFieldHandle, $subFieldInfo, $blockIndex, $blockHandle, $fieldData);
+            $parsedValue = $this->_parseSubField($nodePaths, $subFieldHandle, $subFieldInfo);
+
+            if (isset($fieldData[$key])) {
+                $fieldData[$key] = array_merge_recursive($fieldData[$key], $parsedValue);
+            } else {
+                $fieldData[$key] = $parsedValue;
+            }
         }
 
         ksort($fieldData, SORT_NUMERIC);
@@ -144,7 +151,43 @@ class Matrix extends Field implements FieldInterface
     // Private Methods
     // =========================================================================
 
-    private function _parseSubField($feedData, $subFieldHandle, $subFieldInfo, $blockIndex, $blockHandle, $fieldData)
+    private function _getFieldMappingInfoForNodePath($nodePath, $blocks)
+    {
+        foreach ($blocks as $blockHandle => $blockInfo) {
+            $fields = Hash::get($blockInfo, 'fields');
+
+            $feedPath = preg_replace('/(\/\d+\/)/', '/', $nodePath);
+            $feedPath = preg_replace('/^(\d+\/)|(\/\d+)/', '', $feedPath);
+
+            foreach ($fields as $subFieldHandle => $subFieldInfo) {
+                $node = Hash::get($subFieldInfo, 'node');
+
+                $isComplexField = Hash::extract($subFieldInfo, 'fields.{*}.node');
+
+                if (in_array($feedPath, $isComplexField)) {
+                    return [
+                        'blockHandle' => $blockHandle,
+                        'subFieldHandle' => $subFieldHandle,
+                        'subFieldInfo' => $subFieldInfo,
+                        'nodePath' => $nodePath,
+                        'isComplexField' => true,
+                    ];
+                }
+
+                if ($feedPath == $node) {
+                    return [
+                        'blockHandle' => $blockHandle,
+                        'subFieldHandle' => $subFieldHandle,
+                        'subFieldInfo' => $subFieldInfo,
+                        'nodePath' => $nodePath,
+                        'isComplexField' => false,
+                    ];
+                }
+            }
+        }
+    }
+
+    private function _parseSubField($feedData, $subFieldHandle, $subFieldInfo)
     {
         $subFieldClassHandle = Hash::get($subFieldInfo, 'field');
 
@@ -161,25 +204,6 @@ class Matrix extends Field implements FieldInterface
         // Get our content, parsed by this fields service function
         $parsedValue = $class->parseField();
 
-        // Create a new index with 'new1' plus the sub-field handle
-        $blockSubFieldHandle = ($blockIndex + 1) . '.' . $blockHandle . '.' . $subFieldHandle;
-
-        // Store and we're done
-        return array_merge_recursive($fieldData, [$blockSubFieldHandle => $parsedValue]);
+        return $parsedValue;
     }
-
-    // private function _parseNodeTree(&$tree, $array, $index = '') {
-    //     foreach ($array as $key => $value) {
-    //         $node = Hash::get($value, 'node');
-    //         $fields = Hash::get($value, 'fields');
-
-    //         if ($node) {
-    //             $tree[$node] = $index . $key;
-    //         }
-
-    //         if ($fields) {
-    //             $this->_parseNodeTree($tree, $fields, $index . $key . '.fields.');
-    //         }
-    //     }
-    // }
 }
