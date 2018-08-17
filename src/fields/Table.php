d86a40c6234b443cd9b51dde28ec3957a50473fb
Josh Crawford <joshua@sgroup.com.au>
2018-08-17 23:09:47 +1000

    Fix table field when no delimiter defined

--- src/fields/Table.php
+++ src/fields/Table.php
@@ -71,11 +71,15 @@ class Table extends Field implements FieldInterface
 
         foreach ($parsedData as $rowKey => $row) {
             foreach ($row as $columnKey => $column) {
-                $columnValues = explode($dataDelimiter, $column);
+                if (is_string($column)) {
+                    $columnValues = explode($dataDelimiter, $column);
 
-                if (count($columnValues) > 1) {
-                    foreach ($columnValues as $splitRowKey => $columnValue) {
-                        $preppedData[$splitRowKey][$columnKey] = $columnValue;
+                    if (count($columnValues) > 1) {
+                        foreach ($columnValues as $splitRowKey => $columnValue) {
+                            $preppedData[$splitRowKey][$columnKey] = $columnValue;
+                        }
+                    } else {
+                        $preppedData[$rowKey][$columnKey] = $column;
                     }
                 } else {
                     $preppedData[$rowKey][$columnKey] = $column;
