266c841bc968f751ce31ecd44592f49808ead14d
Josh Crawford <joshua@sgroup.com.au>
2018-08-17 15:18:44 +1000

    version 2.0.13

--- feedme/FeedMePlugin.php
+++ feedme/FeedMePlugin.php
@@ -19,7 +19,7 @@ class FeedMePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '2.0.12';
+        return '2.0.13';
     }
 
     public function getSchemaVersion()
