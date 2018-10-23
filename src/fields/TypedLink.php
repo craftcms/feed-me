fe7b745c57370c143b030c088b1ae68c0f981f9a
Josh Crawford <joshua@sgroup.com.au>
2018-10-23 16:05:54 +1100

    Typed link field support

---
+++ src/fields/TypedLink.php
@@ -0,0 +1,48 @@
+<?php
+namespace verbb\feedme\fields;
+
+use verbb\feedme\FeedMe;
+use verbb\feedme\base\Field;
+use verbb\feedme\base\FieldInterface;
+use verbb\feedme\helpers\DataHelper;
+
+use Craft;
+use craft\helpers\Localization;
+
+use Cake\Utility\Hash;
+
+class TypedLink extends Field implements FieldInterface
+{
+    // Properties
+    // =========================================================================
+
+    public static $name = 'TypedLink';
+    public static $class = 'typedlinkfield\fields\LinkField';
+
+
+    // Templates
+    // =========================================================================
+
+    public function getMappingTemplate()
+    {
+        return 'feed-me/_includes/fields/typed-link';
+    }
+
+
+    // Public Methods
+    // =========================================================================
+
+    public function parseField()
+    {
+        $preppedData = [];
+
+        $fields = Hash::get($this->fieldInfo, 'fields');
+
+        foreach ($fields as $subFieldHandle => $subFieldInfo) {
+            $preppedData[$subFieldHandle] = DataHelper::fetchValue($this->feedData, $subFieldInfo);
+        }
+
+        return $preppedData;
+    }
+
+}
