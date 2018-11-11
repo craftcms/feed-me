44deea37e49a76ee3ff1150013fb29f6b59463d4
Josh Crawford <joshua@sgroup.com.au>
2018-11-12 09:58:39 +1100

    Fix enabled tabs throwing an error in some cases

--- src/web/twig/variables/FeedMeVariable.php
+++ src/web/twig/variables/FeedMeVariable.php
@@ -40,7 +40,7 @@ class FeedMeVariable extends ServiceLocator
             'settings' => [ 'label' => Craft::t('feed-me', 'Settings'), 'url' => UrlHelper::cpUrl('feed-me/settings') ],
         ];
 
-        if ($enabledTabs === '*' || $enabledTabs === 1) {
+        if ($enabledTabs === '*' || $enabledTabs === 1 || !is_array($enabledTabs)) {
             return $tabs;
         }
 
