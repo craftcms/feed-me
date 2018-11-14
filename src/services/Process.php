b0ba1f624bcbe4d671930220e3ed9f67bb4f98d4
Josh Crawford <joshua@sgroup.com.au>
2018-11-14 22:04:11 +1100

    Fix content checks not compating existing content correctly

--- src/services/Process.php
+++ src/services/Process.php
@@ -228,7 +228,7 @@ class Process extends Component
 
             // If we're deleting or updating an existing element, we want to focus on that one
             if (DuplicateHelper::isUpdate($feed)) {
-                $element = $existingElement;
+                $element = clone $existingElement;
 
                 // Update our service with the correct element
                 $this->_service->element = $element;
