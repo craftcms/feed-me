7b37157fe070068140b460e8adf6190fe626c771
Josh Crawford <joshua@sgroup.com.au>
2019-02-14 21:45:49 +1100

    Implement log file rotation - stop them getting out of hand

--- src/services/Logs.php
+++ src/services/Logs.php
@@ -14,7 +14,13 @@ class Logs extends Component
     // Properties
     // =========================================================================
 
-    private $_logFile;
+    public $enableRotation = true;
+    public $maxFileSize = 1024; // in KB
+    public $maxLogFiles = 20;
+    public $fileMode;
+    public $dirMode = 0775;
+    public $rotateByCopy = true;
+    public $logFile;
 
 
     // Public Methods
@@ -22,7 +28,7 @@ class Logs extends Component
 
     public function init()
     {
-        $this->_logFile = Craft::$app->path->getLogPath() . '/feedme.log';
+        $this->logFile = Craft::$app->path->getLogPath() . '/feedme.log';
     }
 
     public function log($method, $message, $params = [], $options = [])
@@ -55,16 +61,12 @@ class Logs extends Component
 
         $options = json_encode($options);
 
-        $fp = fopen($this->_logFile, 'ab');
-        fwrite($fp, $options . PHP_EOL);
-        fclose($fp);
+        $this->export($options . PHP_EOL);
     }
 
     public function clear()
     {
-        if (@file_exists($this->_logFile)) {
-            FileHelper::unlink($this->_logFile);
-        }
+        $this->clearLogFile($this->logFile);
     }
 
     public function getLogEntries($type = null): array
@@ -76,9 +78,9 @@ class Logs extends Component
         if (@file_exists(Craft::$app->path->getLogPath())) {
             $logEntries = [];
 
-            if (@file_exists($this->_logFile)) {
+            if (@file_exists($this->logFile)) {
                 // Split the log file's contents up into arrays where every line is a new item
-                $contents = @file_get_contents($this->_logFile);
+                $contents = @file_get_contents($this->logFile);
                 $lines = explode("\n", $contents);
 
                 foreach ($lines as $line) {
@@ -136,5 +138,92 @@ class Logs extends Component
         }
 
         return true;
+    }
+
+    private function export($text)
+    {
+        $logPath = dirname($this->logFile);
+        FileHelper::createDirectory($logPath, $this->dirMode, true);
+
+        if (($fp = @fopen($this->logFile, 'a')) === false) {
+            throw new \Exception("Unable to append to log file: {$this->logFile}");
+        }
+        @flock($fp, LOCK_EX);
+        if ($this->enableRotation) {
+            // clear stat cache to ensure getting the real current file size and not a cached one
+            // this may result in rotating twice when cached file size is used on subsequent calls
+            clearstatcache();
+        }
+        if ($this->enableRotation && @filesize($this->logFile) > $this->maxFileSize * 1024) {
+            $this->rotateFiles();
+            @flock($fp, LOCK_UN);
+            @fclose($fp);
+            $writeResult = @file_put_contents($this->logFile, $text, FILE_APPEND | LOCK_EX);
+            if ($writeResult === false) {
+                $error = error_get_last();
+                throw new \Exception("Unable to export log through file!: {$error['message']}");
+            }
+            $textSize = strlen($text);
+            if ($writeResult < $textSize) {
+                throw new \Exception("Unable to export whole log through file! Wrote $writeResult out of $textSize bytes.");
+            }
+        } else {
+            $writeResult = @fwrite($fp, $text);
+            if ($writeResult === false) {
+                $error = error_get_last();
+                throw new \Exception("Unable to export log through file!: {$error['message']}");
+            }
+            $textSize = strlen($text);
+            if ($writeResult < $textSize) {
+                throw new \Exception("Unable to export whole log through file! Wrote $writeResult out of $textSize bytes.");
+            }
+            @flock($fp, LOCK_UN);
+            @fclose($fp);
+        }
+        if ($this->fileMode !== null) {
+            @chmod($this->logFile, $this->fileMode);
+        }
+    }
+
+    private function rotateFiles()
+    {
+        $file = $this->logFile;
+        for ($i = $this->maxLogFiles; $i >= 0; --$i) {
+            // $i == 0 is the original log file
+            $rotateFile = $file . ($i === 0 ? '' : '.' . $i);
+            if (is_file($rotateFile)) {
+                // suppress errors because it's possible multiple processes enter into this section
+                if ($i === $this->maxLogFiles) {
+                    @unlink($rotateFile);
+                    continue;
+                }
+                $newFile = $this->logFile . '.' . ($i + 1);
+                $this->rotateByCopy ? $this->rotateByCopy($rotateFile, $newFile) : $this->rotateByRename($rotateFile, $newFile);
+                if ($i === 0) {
+                    $this->clearLogFile($rotateFile);
+                }
+            }
+        }
+    }
+
+    private function clearLogFile($rotateFile)
+    {
+        if ($filePointer = @fopen($rotateFile, 'a')) {
+            @ftruncate($filePointer, 0);
+            @fclose($filePointer);
+        }
+    }
+
+    private function rotateByCopy($rotateFile, $newFile)
+    {
+        @copy($rotateFile, $newFile);
+        if ($this->fileMode !== null) {
+            @chmod($newFile, $this->fileMode);
+        }
+    }
+
+    private function rotateByRename($rotateFile, $newFile)
+    {
+        @rename($rotateFile, $newFile);
     }
 }
