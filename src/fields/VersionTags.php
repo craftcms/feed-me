db3d626f1fa96128cb9ebd50472558e5b207fe65
KSP <kristian.palshaugen@gmail.com>
2020-07-31 10:27:36 +0200

    Adding support for VersionTags field type

---
+++ src/fields/VersionTags.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace craft\feedme\fields;
+
+class VersionTags extends Tags
+{
+    // Properties
+    // =========================================================================
+
+    public static $name = 'VersionTags';
+    public static $class = 'ilkino\cinemaprogram\fields\VersionTags';
+    public static $elementType = 'craft\elements\Tag';
+}
