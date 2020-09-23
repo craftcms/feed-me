3ffe0bb02d7ba311882dc4bce0f43be84354cea6
Matt Stein <matt@pixelandtonic.com>
2020-09-23 18:40:03 -0500

    Update config.
    - Update format for current VuePress version.
    - Remove unused `replaceApiLinks()`.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
+    title: 'Feed Me Documentation',
     theme: 'craftdocs',
-    ga: 'UA-39036834-16',
+    plugins: [["@vuepress/google-analytics", { ga: "UA-39036834-16" }]],
     base: '/feed-me/v4/',
     shouldPrefetch: () => false,
     lang: 'en-US',
-    title: 'Feed Me Documentation',
     themeConfig: {
         editLinks: true,
         docsRepo: 'craftcms/feed-me',
@@ -109,88 +109,9 @@ module.exports = {
                 return content.replace(/[_`]/g, '')
             }
         },
-        config(md) {
-            md
-                .use(replaceApiLinks)
-                .use(require('vuepress-theme-craftdocs/markup'))
-                .use(require('markdown-it-deflist'))
-        }
+        extendMarkdown(md) {
+            require("vuepress-theme-craftdocs/markup")(md);
+            md.use(require("markdown-it-deflist"));
+        },
     },
-}
-
-function replaceApiLinks(md) {
-    // code adapted from the markdown-it-replace-link plugin
-    md.core.ruler.after(
-        'inline',
-        'replace-link',
-        function (state) {
-            state.tokens.forEach(function (blockToken) {
-                if (blockToken.type === 'inline' && blockToken.children) {
-                    blockToken.children.forEach(function (token, tokenIndex) {
-                        if (token.type === 'link_open') {
-                            token.attrs.forEach(function (attr) {
-                                if (attr[0] === 'href') {
-                                    let replace = replaceApiLink(attr[1]);
-                                    if (replace) {
-                                        attr[1] = replace;
-                                        let next = blockToken.children[tokenIndex+1];
-                                        if (next.type === 'text') {
-                                            next.content = next.content.replace(/^(api|config):/, '');
-                                        }
-                                    }
-                                }
-                                return false;
-                            });
-                        }
-                    });
-                }
-            });
-            return false;
-        }
-    );
-}
-
-function replaceApiLink(link) {
-    link = decodeURIComponent(link)
-    let m = link.match(/^(?:api:)?\\?([\w\\]+)(?:::\$?(\w+)(\(\))?)?(?:#([\w\-]+))?$/)
-    if (m) {
-        let className = m[1]
-        let subject = m[2]
-        let isMethod = typeof m[3] !== 'undefined'
-        let hash = m[4]
-
-        if (className.match(/^craft\\commerce\\/)) {
-            let url = 'https://docs.craftcms.com/commerce/api/v2/'+className.replace(/\\/g, '-').toLowerCase()+'.html'
-            if (subject) {
-                hash = ''
-                if (isMethod) {
-                    hash = 'method-'
-                } else if (!subject.match(/^EVENT_/)) {
-                    hash = 'property-'
-                }
-                hash += subject.replace(/_/g, '-').toLowerCase()
-            }
-            return url + (hash ? `#${hash}` : '');
-        }
-
-        if (className.match(/^craft\\/) || className.match(/^Craft/)) {
-            let url = 'https://docs.craftcms.com/api/v3/'+className.replace(/\\/g, '-').toLowerCase()+'.html'
-            if (subject) {
-                hash = ''
-                if (isMethod) {
-                    hash = 'method-'
-                }
-                hash += subject.replace(/_/g, '-').toLowerCase()
-            }
-            return url + (hash ? `#${hash}` : '');
-        }
-
-        if (className.match(/^yii\\/) || className.match(/^Yii/)) {
-            let url = 'https://www.yiiframework.com/doc/api/2.0/'+className.replace(/\\/g, '-').toLowerCase()
-            if (subject) {
-                hash = (isMethod ? `${subject}()` : `\$${subject}`)+'-detail'
-            }
-            return url + (hash ? `#${hash}` : '');
-        }
-    }
 }
