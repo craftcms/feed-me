5a75e50a376466dd243e299e15d6049823e1743c
Brad Bell <brad@pixelandtonic.com>
2022-03-07 17:15:50 -0800

    Fixed #692, #825

--- src/web/twig/variables/FeedMeVariable.php
+++ src/web/twig/variables/FeedMeVariable.php
@@ -6,6 +6,7 @@ use Craft;
 use craft\feedme\Plugin;
 use craft\helpers\DateTimeHelper;
 use craft\helpers\UrlHelper;
+use craft\models\Section;
 use yii\di\ServiceLocator;
 
 /**
@@ -203,10 +204,11 @@ class FeedMeVariable extends ServiceLocator
         } else if ($type === 'craft\fields\Categories') {
             $source = $this->getCategorySourcesByField($field);
         } else if ($type === 'craft\fields\Entries') {
+            /** @var Section $section */
             $section = $this->getEntrySourcesByField($field)[0] ?? null;
 
             if ($section) {
-                $source = Craft::$app->sections->getEntryTypeById($section->id);
+                $source = $section->getEntryTypes()[0] ?? null;
             }
         } else if ($type === 'craft\fields\Tags') {
             $source = $this->getTagSourcesByField($field);
