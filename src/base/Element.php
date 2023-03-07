f804fd8e976005acfc057028931528fe53e50744
Iwona Just <iwona@pixelandtonic.com>
2023-03-07 11:09:44 +0000

    don't disable already disabled elements

--- src/base/Element.php
+++ src/base/Element.php
@@ -201,8 +201,10 @@ abstract class Element extends Component implements ElementInterface
         foreach ($elementIds as $elementId) {
             /** @var BaseElement $element */
             $element = $elementsService->getElementById($elementId, $class);
-            $element->enabled = false;
-            $elementsService->saveElement($element, true, true, Hash::get($this->feed, 'updateSearchIndexes'));
+            if ($element->enabled) {
+                $element->enabled = false;
+                $elementsService->saveElement($element, true, true, Hash::get($this->feed, 'updateSearchIndexes'));
+            }
         }
 
         return true;
@@ -226,8 +228,10 @@ abstract class Element extends Component implements ElementInterface
 
         foreach ($query->each() as $element) {
             /** @var BaseElement $element */
-            $element->enabledForSite = false;
-            $elementsService->saveElement($element, false, false, Hash::get($this->feed, 'updateSearchIndexes'));
+            if ($element->enabledForSite) {
+                $element->enabledForSite = false;
+                $elementsService->saveElement($element, false, false, Hash::get($this->feed, 'updateSearchIndexes'));
+            }
         }
 
         return true;
