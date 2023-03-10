20d05bb68f8cb3e9b508a39c799d0762f4764b5d
Iwona Just <iwona@pixelandtonic.com>
2023-03-10 11:14:48 +0000

    newParentId -> parentId

--- src/elements/Entry.php
+++ src/elements/Entry.php
@@ -225,7 +225,7 @@ class Entry extends Element
                 Plugin::error('Entry error: Could not create parent - `{e}`.', ['e' => Json::encode($element->getErrors())]);
             } else {
                 Plugin::info('Entry `#{id}` added.', ['id' => $element->id]);
-                $this->element->newParentId = $element->id;
+                $this->element->parentId = $element->id;
             }
 
             return $element->id;
