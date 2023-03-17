65aa10e6d14bf467551d4816a2a947c2804c4986
Tim Kelty <tim@craftcms.com>
2023-03-17 14:53:58 -0400

    Cleanup

--- src/web/assets/feedme/webpack.config.js
+++ src/web/assets/feedme/webpack.config.js
@@ -9,6 +9,18 @@ module.exports = getConfig({
     entry: {
       FeedMe: './FeedMe.js',
     },
+    module: {
+      // Remove font rule once https://github.com/craftcms/cms/pull/12923 is merged
+      rules: [
+        {
+          test: /fonts\/[a-zA-Z0-9\-\_]*\.(ttf|woff|woff2|svg|eot)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext][query]',
+          },
+        },
+      ]
+    },
     plugins: [
       new CopyWebpackPlugin({
         patterns: [
