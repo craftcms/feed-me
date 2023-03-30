dc37ff7bc1a5a05d77453da2c227bc3d14ec2185
preciousdaleramirez <dalefirstpage@gmail.com>
2023-03-30 16:25:31 +0800

    Fixed a bug where importing data to the commerce variant table field don’t work.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Release Notes for Feed Me 4.x
 
+### Fixed
+- Fixed a bug where importing data to the commerce variant table field don’t work. ([#464](https://github.com/craftcms/feed-me/issues/464))
+
 ## 4.6.1 - 2023-03-20
 
 - Fixed a JavaScript error that would occur on case-sensitive filesystems when using Feed Me. ([#1260](https://github.com/craftcms/feed-me/pull/1260), [#1257](https://github.com/craftcms/feed-me/issues/1257), [#1258](https://github.com/craftcms/feed-me/issues/1258), [#1259](https://github.com/craftcms/feed-me/issues/1259))
--- src/elements/CommerceProduct.php
+++ src/elements/CommerceProduct.php
@@ -14,6 +14,8 @@ use craft\feedme\helpers\BaseHelper;
 use craft\feedme\helpers\DataHelper;
 use craft\feedme\Plugin;
 use craft\feedme\services\Process;
+use craft\fields\Matrix;
+use craft\fields\Table;
 use yii\base\Event;
 
 /**
@@ -310,9 +312,9 @@ class CommerceProduct extends Element
                     }
                 }
 
-                $isMatrixField = (Hash::get($fieldInfo, 'field') === 'craft\fields\Matrix');
+                $isNestedField = (in_array(Hash::get($fieldInfo, 'field'), [Matrix::class, Table::class]));
 
-                if ($isMatrixField) {
+                if ($isNestedField === true) {
                     $complexFields[$variantIndex][$fieldHandle]['info'] = $fieldInfo;
                     $complexFields[$variantIndex][$fieldHandle]['data'][$nodePath] = $value;
                     continue;
