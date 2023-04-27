0cdb5a16c29c88c685ff7d7ab372a8777bec9850
Iwona Just <iwona@pixelandtonic.com>
2023-04-27 09:55:54 +0100

    check if user element before setting preferred locale

--- src/services/Process.php
+++ src/services/Process.php
@@ -5,6 +5,7 @@ namespace craft\feedme\services;
 use Cake\Utility\Hash;
 use Craft;
 use craft\base\Component;
+use craft\elements\User;
 use craft\errors\ShellCommandException;
 use craft\feedme\base\ElementInterface;
 use craft\feedme\events\FeedProcessEvent;
@@ -475,7 +476,7 @@ class Process extends Component
         if ($this->_service->save($element, $feed)) {
 
             // save user's preferences only after user has been successfully saved
-            if (isset($attributeData['preferredLocale'])) {
+            if ($element instanceof User && isset($attributeData['preferredLocale'])) {
                 if (!empty($attributeData['preferredLocale']) || $feed['setEmptyValues'] === 1) {
                     $preferences = ['locale' => $attributeData['preferredLocale']];
                     Craft::$app->getUsers()->saveUserPreferences($element, $preferences);
