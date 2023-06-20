41fb24ca9dca3c03eec477982065534535a872b2
Iwona Just <iwona@pixelandtonic.com>
2023-06-20 08:48:15 +0100

    check if element enabled(forSite) attribute(s) changed

--- src/services/Process.php
+++ src/services/Process.php
@@ -372,6 +372,7 @@ class Process extends Component
         // Set the attributes for the element
         $element->setAttributes($attributeData, false);
 
+        $contentData = [];
         if (isset($attributeData['enabled'])) {
             // Set the site-specific status as well, but retain all other site statuses
             $enabledForSite = [];
@@ -387,6 +388,8 @@ class Process extends Component
             // Set the global status to true if it's enabled for *any* sites, or if already enabled.
             $element->enabled = in_array(true, $enabledForSite) || $element->enabled;
             $element->setEnabledForSite($enabledForSite);
+            $contentData['enabled'] = $element->enabled;
+            $contentData['enabledForSite'] = $element->getEnabledForSite($element->siteId);
         }
 
         // Then, do the same for custom fields. Again, this should be done after populating the element attributes
@@ -424,7 +427,7 @@ class Process extends Component
         }
 
         // We need to keep these separate to apply to the element but required when matching against existing elements
-        $contentData = $attributeData + $fieldData;
+        $contentData += $attributeData + $fieldData;
 
         //
         // It's time to actually save the element!
