63ad12e59f85a8c4c6fc908cada7055c598a4bbf
i-just <iwona@pixelandtonic.com>
2025-04-04 15:38:41 +0100

    don't rely on on array contains being the same as actually comparing arrays

--- src/helpers/DataHelper.php
+++ src/helpers/DataHelper.php
@@ -264,13 +264,6 @@ class DataHelper
         foreach ($content as $key => $newValue) {
             $existingValue = Hash::get($fields, $key);
 
-            [$existingValue, $newValue] = self::prepDatesForComparison($existingValue, $newValue);
-
-            // If array key & values are already within the existing array
-            if (is_array($newValue) && is_array($existingValue) && Hash::contains($existingValue,$newValue)) {
-                unset($trackedChanges[$key]);
-                continue;
-            }
 
             // Check for simple fields first
             if (self::_compareSimpleValues($fields, $key, $existingValue, $newValue)) {
